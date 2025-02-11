30a8525e663598b95a9a6e516ab8c1b78e93091b
Oscdivin <divineuwandu84@Gmail.com>
2025-02-11 01:06:39 +0100

    Header Navbar is done now

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,21 +1,28 @@
+import { assets } from "../../assets/frontend_assets/assets"
 
 const Navbar = () => {
     return (
-        <div>
-            <div>
-                <img src="" alt="" />
-                <ul>
+        <header className="flex justify-center items-center w-full h-[12vh]  top-0 z-20">
+            <div className="flex justify-between items-center w-[88%]">
+                <img src={assets.logo} alt="" />
+                <ul className="flex space-x-8 justify-center items-center text-[#49557e] text-[17px]">
                     <li>Home</li>
                     <li>Menu</li>
                     <li>Mobile app</li>
                     <li>Contact Us</li>
                 </ul>
 
-                <div>
+                <div className="flex space-x-9 justify-center items-center">
+                    <img src={assets.search_icon} alt="" />
+                    <div>
+                        <img src={assets.basket_icon} alt="" />
+                        <div></div>
+                    </div>
+                    <button className="px-[25px] py-[7px] rounded-full text-[#49557e] border border-solid cursor-pointer transition duration-300 hover:bg-[#fff4f2]">Sign-In</button>
 
                 </div>
             </div>
-        </div>
+        </header>
     )
 }
 
