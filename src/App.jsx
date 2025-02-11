1ef69250f754c679ac3a65b197bd6056d06e8321
Oscdivin <divineuwandu84@Gmail.com>
2025-02-11 14:07:17 +0100

    added Routes and

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { Route, Routes } from "react-router-dom"
 import Footer from "./components/common/Footer"
 import Navbar from "./components/common/Navbar"
 import HomeScreen from "./pages/Homescreen"
+import Cat from "./pages/Cart/Cat"
+import PlaceOrder from "./pages/placeOrder/PlaceOrder"
 // import HomeScreen from "./pages/HomeScreen"
 
 const App = () => {
   return (
     <div >
       <Navbar />
-      <HomeScreen />
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/cart" element={<Cat />} />
+        <Route path="/order" element={<PlaceOrder />} />
+      </Routes>
       <Footer />
 
     </div>
